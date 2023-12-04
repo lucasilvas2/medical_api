64d15d6252ad8cb3e9579b0e6fe815e8c2a58701
LucasPC <lucasoliver1068@gmail.com>
2023-12-04 20:05:27 -0300

    first commit

---
+++ src/main/java/com/imd/medical_api/MedicalApiApplication.java
@@ -0,0 +1,13 @@
+package com.imd.medical_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalApiApplication.class, args);
+    }
+
+}
