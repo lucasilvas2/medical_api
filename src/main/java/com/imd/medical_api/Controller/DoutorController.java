9a8751f91fcc8a8ea98b3baa6ad1de9be1a3d7ad
Fabricio <fabriciocruzc@gmail.com>
2023-12-06 20:21:13 -0300

    CRUD doutor

--- src/main/java/com/imd/medical_api/Controller/DoutorController.java
+++ src/main/java/com/imd/medical_api/Controller/DoutorController.java
@@ -1,13 +1,14 @@
 package com.imd.medical_api.Controller;
 
 import com.imd.medical_api.domain.doutor.DoutorCreateDTO;
+import com.imd.medical_api.domain.doutor.DoutorUpdateDTO;
+import com.imd.medical_api.model.Doutor;
 import com.imd.medical_api.service.DoutorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = {"/doutores", "doutor"})
@@ -19,5 +20,20 @@ public class DoutorController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DoutorCreateDTO dados){
         doutor.cadastrarDoutor(dados);
+    }
+
+    @GetMapping
+    public List<Doutor> listar(){
+        return doutor.listarDoutores();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDoutor(@PathVariable Long id){
+        doutor.deleteDoutor(id);
+    }
+
+    @PutMapping
+    public void AtualizarDoutor(@RequestBody @Valid DoutorUpdateDTO dados){
+        doutor.atualizarDoutor(dados);
     }
 }
--- src/main/java/com/imd/medical_api/service/DoutorService.java
+++ src/main/java/com/imd/medical_api/service/DoutorService.java
@@ -1,12 +1,17 @@
 package com.imd.medical_api.service;
 
 import com.imd.medical_api.domain.doutor.DoutorCreateDTO;
+import com.imd.medical_api.domain.doutor.DoutorDetailDTO;
+import com.imd.medical_api.domain.doutor.DoutorUpdateDTO;
 import com.imd.medical_api.model.Doutor;
 import com.imd.medical_api.repository.DoutorRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DoutorService {
@@ -17,5 +22,29 @@ public class DoutorService {
     public ResponseEntity cadastrarDoutor(DoutorCreateDTO dados){
         repository.save(new Doutor(dados));
         return ResponseEntity.noContent().build();
+    }
+
+
+    public List<Doutor> listarDoutores(){
+        return repository.findAll();
+    }
+
+    @Transactional
+    public ResponseEntity deleteDoutor(Long id){
+        if(id == null){
+            return ResponseEntity.badRequest().build();
+        }
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Transactional
+    public ResponseEntity atualizarDoutor(DoutorUpdateDTO dados){
+        if(dados.id() == null){
+            return ResponseEntity.badRequest().build();
+        }
+        Doutor doutor = repository.getReferenceById(dados.id());
+        doutor.update(dados);
+        return ResponseEntity.ok().build();
     }
 }
