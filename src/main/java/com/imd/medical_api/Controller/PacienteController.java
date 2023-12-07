85c379008c431d223c7342100a0061c215f96a2e
Fabricio <fabriciocruzc@gmail.com>
2023-12-07 19:31:40 -0300

    Criando CRUD paciente

--- src/main/java/com/imd/medical_api/Controller/PacienteController.java
+++ src/main/java/com/imd/medical_api/Controller/PacienteController.java
@@ -1,4 +1,45 @@
 package com.imd.medical_api.Controller;
 
+
+import com.imd.medical_api.domain.doutor.DoutorCreateDTO;
+import com.imd.medical_api.domain.doutor.DoutorUpdateDTO;
+import com.imd.medical_api.domain.paciente.PacienteCreateDTO;
+import com.imd.medical_api.domain.paciente.PacienteUpdateDTO;
+import com.imd.medical_api.model.Doutor;
+import com.imd.medical_api.model.Paciente;
+import com.imd.medical_api.service.DoutorService;
+import com.imd.medical_api.service.PacienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = {"/paciente"})
 public class PacienteController {
+
+    @Autowired
+    PacienteService paciente;
+
+    @PostMapping
+    public void cadastrar(@RequestBody @Valid PacienteCreateDTO dados){
+        paciente.cadastrarPaciente(dados);
+    }
+
+    @GetMapping
+    public List<Paciente> listar(){
+        return paciente.listarPaciente();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePaciente(@PathVariable Long id){
+        paciente.deletePaciente(id);
+    }
+
+    @PutMapping
+    public void AtualizarPacienter(@RequestBody @Valid PacienteUpdateDTO dados){
+        paciente.atualizarPaciente(dados);
+    }
+
 }
--- src/main/java/com/imd/medical_api/service/PacienteService.java
+++ src/main/java/com/imd/medical_api/service/PacienteService.java
@@ -1,4 +1,50 @@
 package com.imd.medical_api.service;
 
+
+import com.imd.medical_api.domain.doutor.DoutorCreateDTO;
+import com.imd.medical_api.domain.doutor.DoutorUpdateDTO;
+import com.imd.medical_api.domain.paciente.PacienteCreateDTO;
+import com.imd.medical_api.domain.paciente.PacienteUpdateDTO;
+import com.imd.medical_api.model.Doutor;
+import com.imd.medical_api.model.Paciente;
+import com.imd.medical_api.repository.PacienteRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PacienteService {
+    @Autowired
+    PacienteRepository repository;
+
+    public ResponseEntity cadastrarPaciente(PacienteCreateDTO dados){
+        repository.save(new Paciente(dados));
+        return ResponseEntity.noContent().build();
+    }
+
+    public List<Paciente> listarPaciente(){
+        return repository.findAll();
+    }
+
+    @Transactional
+    public ResponseEntity deletePaciente(Long id){
+        if(id == null){
+            return ResponseEntity.badRequest().build();
+        }
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Transactional
+    public ResponseEntity atualizarPaciente(PacienteUpdateDTO dados){
+        if(dados.id() == null){
+            return ResponseEntity.badRequest().build();
+        }
+        Paciente paciente = repository.getReferenceById(dados.id());
+        paciente.update(dados);
+        return ResponseEntity.ok().build();
+    }
 }
