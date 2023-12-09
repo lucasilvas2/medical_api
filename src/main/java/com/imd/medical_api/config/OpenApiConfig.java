469af1b52f0194e4d33cf9ad5cb4fa8e72bd4c12
Fabriciocruzc <fabriciocruzc@gmail.com>
2023-12-09 14:20:07 -0300

    Ajuste config

--- src/main/java/com/imd/medical_api/config/OpenApiConfig.java
+++ src/main/java/com/imd/medical_api/config/OpenApiConfig.java
@@ -4,16 +4,31 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 
 public class OpenApiConfig {
     @Bean
-    public OpenAPI usersMicroserviceOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Consulta API")
-                        .description("API de gerenciamento de consultas.")
-                        .version("1.0"));
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return  http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(autorized -> autorized
+                        .requestMatchers(HttpMethod.GET, "/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/**").permitAll()
+                        .anyRequest().authenticated()
 
+                ).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
