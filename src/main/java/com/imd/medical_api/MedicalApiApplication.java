1576bd5bf676e5cb124fdeedeed40b79158bbacd
Fabriciocruzc <fabriciocruzc@gmail.com>
2023-12-09 14:23:51 -0300

    Ajuste na class Application

--- src/main/java/com/imd/medical_api/MedicalApiApplication.java
+++ src/main/java/com/imd/medical_api/MedicalApiApplication.java
@@ -3,11 +3,17 @@ package com.imd.medical_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @SpringBootApplication
 public class MedicalApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MedicalApiApplication.class, args);
+        System.out.println(new BCryptPasswordEncoder().encode("imd@2023"));
+        System.out.println(new BCryptPasswordEncoder().encode("admin"));
     }
 
 }
